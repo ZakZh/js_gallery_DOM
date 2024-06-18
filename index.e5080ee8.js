cba171f94c8d1c3a58b331450c003de67b5e9097
ZakZh <38009936+ZakZh@users.noreply.github.com>
2024-06-18 20:43:49 +0300

    make build

---
+++ index.e5080ee8.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("#largeImg");document.querySelector("#thumbs").addEventListener("click",function(t){t.preventDefault(),e.src=t.target.closest(".list-item__link").href});
+//# sourceMappingURL=index.e5080ee8.js.map
